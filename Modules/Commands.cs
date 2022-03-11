c635c6b6a0d2d9fbc346615c51150b63e42a36f6
anas kaaouachi <kawashi.a@hotmail.fr>
2022-03-11 14:43:06 +0100

    ajout de la commande userinfo(incomplète) et ajout de la commande jitsi

--- Modules/Commands.cs
+++ Modules/Commands.cs
@@ -1,4 +1,6 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +16,50 @@ namespace MaraBis.Modules
         {
             await ReplyAsync("Pong !");
         }
+
+        [Command("userinfo")]
+        [Summary("Give information about the user")]
+        [Alias("user", "whois")]
+        public async Task UserInfoAsync(SocketUser user = null)
+        {
+            var userInfo = user ?? Context.Client.CurrentUser;
+            await ReplyAsync($"Username: {userInfo.Username}#{userInfo.Discriminator}");
+            await ReplyAsync($"User Id : <#{userInfo.Id}>");
+            await ReplyAsync($"Avatar : ");
+            await ReplyAsync($"{userInfo.AvatarId}");
+        }
+
+        [Command("jitsi")]
+        [Summary("link to Jitsi")]
+        [Alias("jitsi", "videocall")]
+        public async Task JitsiAsync()
+        {
+            var builder = new ComponentBuilder().WithButton("Vers jitsi", style: ButtonStyle.Link, url: "https://meet.jit.si/DevOps21Technifutur_fai9Ee");
+            await ReplyAsync("Retrouvez Jitsi ici : ", components: builder.Build());
+        }
+    }
+
+    [Group("admin")]
+    public class AdminModule : ModuleBase<SocketCommandContext>
+    {
+        [Group("clean")]
+        public class CleanModule : ModuleBase<SocketCommandContext>
+        {
+            [Command]
+            public async Task DefaultCleanAsync()
+            {
+                // ...
+            }
+
+            [Command("messages")]
+            public async Task CleanAsync(int count)
+            {
+                // ...
+            }
+        }
+
+        [Command("ban")]
+        public Task BanAsync(IGuildUser user) =>
+            Context.Guild.AddBanAsync(user);
     }
 }
